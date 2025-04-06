fe10a0e1d26b61afcb7617626639751cc9d8c5bd
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-04-06 12:55:37 +0530

    feat: add UserTable, Pagination, and SearchBar components with API integration

--- Admin_UI/src/App.jsx
+++ Admin_UI/src/App.jsx
@@ -1,9 +1,44 @@
+import { Pagination } from "./components/Pagination";
+import { SearchBar } from "./components/SearchBar";
+import { UserTable } from "./components/UserTable";
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect, use } from "react";
+
 export const App = () => {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [selectAll, setSelectAll] = useState(false);
+
+  const usersPerPage = 10;
+
+  //get Data from API
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      );
+      console.log(response.data);
+      setUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Admin UI</h1>
-      <p>Welcome to the Admin UI!</p>
+      <SearchBar />
+      <UserTable />
+      <Pagination />
     </div>
   );
 };
---
+++ Admin_UI/src/components/Pagination.jsx
@@ -0,0 +1,7 @@
+export const Pagination = () => {
+  return (
+    <>
+      <h1>Pagination Page</h1>
+    </>
+  );
+};
